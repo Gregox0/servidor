46adcd4fb1316e0ccad78d368055dca24d08b2e4
Daniell w. <126434546+Gregox0@users.noreply.github.com>
2025-02-12 11:15:31 -0300

    Update index.js

--- Servidor/index.js
+++ Servidor/index.js
@@ -4,7 +4,7 @@ import { checarStatus, verificarReservas, realizarReserva } from './src/reservaC
 
 const app = express()
 app.use(express.json())
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(cors())
 
 app.get('/status', checarStatus)
 app.get('/checar', verificarReservas)
