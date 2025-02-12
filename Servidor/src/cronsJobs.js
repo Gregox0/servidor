be890d7a204cb3efea032011ccb103f273e2402f
Daniell w. <126434546+Gregox0@users.noreply.github.com>
2025-02-12 12:00:39 -0300

    Update cronsJobs.js

--- Servidor/src/cronsJobs.js
+++ Servidor/src/cronsJobs.js
@@ -3,16 +3,21 @@ import { limparReservas } from './reservaService.js'
 async function sistema(){
   const agora = new Date()
   const horas = agora.getHours() - 3
+  const check = false
 
   if(horas >= 17 || horas < 12){
     try {
-      await limparReservas()
+      if(!check){
+        await limparReservas()
+      }
+      check = true
       return true
     } catch (error) {
       console.error('Erro ao limpar reservas:', error)
     }
   }else{
     return false
+    check = false
   }
 }
 
