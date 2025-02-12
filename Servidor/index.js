d34a28a42fed822dff7061622cfc3569c68fd1f2
Daniell w. <126434546+Gregox0@users.noreply.github.com>
2025-02-12 11:12:36 -0300

    Update index.js

--- Servidor/index.js
+++ Servidor/index.js
@@ -1,15 +1,15 @@
-import express from 'express';
-import cors from 'cors';
-import { checarStatus, verificarReservas, realizarReserva } from './src/reservaController.js';
+import express from 'express'
+import cors from 'cors'
+import { checarStatus, verificarReservas, realizarReserva } from './src/reservaController.js'
 
-const app = express();
-app.use(express.json());
-app.use(cors());
+const app = express()
+app.use(express.json())
+app.use(cors({ origin: "http://localhost:5173", credentials: true }))
 
-app.get('/status', checarStatus);
-app.get('/checar', verificarReservas);
-app.post('/reservar', realizarReserva);
+app.get('/status', checarStatus)
+app.get('/checar', verificarReservas)
+app.post('/reservar', realizarReserva)
 
 app.listen(5555, () => {
-  console.log('Servidor rodando na porta 5555');
+  console.log('Servidor rodando na porta 5555')
 });
