5fffad904d186e5a87651c3be11de2a530bbbab4
Daniell w. <126434546+Gregox0@users.noreply.github.com>
2025-02-12 13:18:28 -0300

    Update cronsJobs.js

--- Servidor/src/cronsJobs.js
+++ Servidor/src/cronsJobs.js
@@ -3,7 +3,6 @@ import { limparReservas } from './reservaService.js'
 async function sistema(){
   const agora = new Date()
   const horas = agora.getHours() - 3
-  let check = false
 
   if(horas >= 17 || horas < 9){
     try {
